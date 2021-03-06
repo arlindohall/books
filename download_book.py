40a30ce9c8eebd9ae4b57cd8125a459b8e930940
Miller Hall <hall@hey.com>
2021-03-06 15:53:13 -0500

    Add library of congress call

--- download_book.py
+++ download_book.py
@@ -3,33 +3,58 @@ import requests
 
 class Book:
     def __init__(self):
-        self.title = None
-        self.isbn = None
-        self.genre = None
+        self.title = ''
+        self.isbn = ''
+        self.genre = ''
         self.categories = []
-        self.lcn = None
+        self.lcn = ''
 
     def to_csv(self) -> str:
+        category = ''
+        if self.categories:
+            category = self.categories[0]
         return ','.join([
             self.isbn,
             self.title,
             self.genre,
-            self.categories[0],
+            category,
             self.lcn,
         ])
 
 class BookClient:
     def __init__(self):
         self.protocol = 'https'
-        self.address = 'www.googleapis.com'
-        self.path = '/books/v1/volumes'
-    def fetch(self, isbn):
-        return requests.get(
-            f'{self.protocol}://{self.address}{self.path}?q={isbn}'
-        )
+
+        self.google_address = 'www.googleapis.com'
+        self.google_path = '/books/v1/volumes'
+
+        self.lc_address = 'www.loc.gov'
+        self.lc_path = '/search'
+
+    def fetch_google(self, isbn):
+        result = requests.get(
+            f'{self.protocol}://{self.google_address}{self.google_path}?q={isbn}'
+        ).json()
+
+        book = Book()
+        if not result['items']:
+            return book
+        else:
+            best_match = result['items'][0]
+
+        book.isbn = isbn
+        # book.title = result['']
+        return book
+
+    def fetch_lc_class(self, isbn):
+        result = requests.get(
+            f'{self.protocol}://{self.lc_address}{self.lc_path}?fo=json&q={isbn}'
+        ).json()
+
+        return ''
 
 def book_for_id(isbn):
-    return BookClient().fetch(isbn)
+    return BookClient().fetch_google(isbn)
 
-id = '0465026567'
-print(book_for_id(id))
+id = input()
+print(book_for_id(id).to_csv())
